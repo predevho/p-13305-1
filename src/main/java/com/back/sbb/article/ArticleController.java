557bf7b52139a7109825a18a2ebba3173c1bfa5b
predevho <dlagusgh7894@gmail.com>
2026-05-04 16:49:18 +0900

    [Feat] 등록 버튼 및 등록 폼 구현

--- src/main/java/com/back/sbb/article/ArticleController.java
+++ src/main/java/com/back/sbb/article/ArticleController.java
@@ -3,13 +3,20 @@ package com.back.sbb.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/article")
 public class ArticleController {
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String list(){
         return "article_list";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "article_create";
     }
 }
